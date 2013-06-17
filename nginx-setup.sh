29f9b21293097deb2553194737e49c599f50032f
lossas <lossas85@gmail.com>
2013-06-17 12:14:30 +0200

    undo changes

--- nginx-setup.sh
+++ nginx-setup.sh
@@ -42,7 +42,6 @@ gsed -i "s/crowdpark/$USER/" conf/nginx/nginx.conf
 cp conf/nginx/nginx.conf /usr/local/etc/nginx/nginx.conf
 
 find /usr/local/etc -name php\*fpm.conf -exec gsed -i 's/daemonize.*/daemonize = yes ; defaul is no/' {} \;
-find /usr/local/etc -name nginx\nginx.conf -exec gsed -i 's/daemonize.*/daemonize = yes ; defaul is no/' {} \;
 
 mkdir -p /usr/local/var/run
 mkdir -p /usr/local/var/log

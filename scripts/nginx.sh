b5be8b08c3b889bfb842ea86c314008361eb2a29
lossas <lossas85@gmail.com>
2013-06-17 11:37:07 +0200

    remove sudo from each command

--- scripts/nginx.sh
+++ scripts/nginx.sh
@@ -5,15 +5,15 @@ executable=/usr/local/sbin/$daemon
 
 case "$1" in
 start)
-   sudo $executable
+   $executable
    [ $? -eq 0 ] && echo "$daemon started..."
 ;;
 stop)
-   sudo $executable -s stop
+   $executable -s stop
    [ $? -eq 0 ] && echo "$daemon stopped..."
 ;;
 reload)
-   sudo $executable -s reload
+   $executable -s reload
    [ $? -eq 0 ] && echo "$daemon reloaded..."
 ;;
 restart)
